98e3147d2588f74df5644ab57aa469e45ef2725a
Mike Wickett <mike@sagecomm.com>
2016-03-15 22:59:29 -0400

    Moved token to private file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var SlackBot = require('slackbots');
+var token_data = require('priv-data');
 
 //create a bot
 // See npmjs.com/package/slackbots
 var bot = new SlackBot({
-  token: 'xoxb-27033721031-qq7eszKVbRemOvVQvRYAJJM0',
+  token: token_data,
   name: 'Squarestatus'
 });
