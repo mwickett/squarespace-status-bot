a23af5cc7b238a4e78cc87e73525ec779613c89a
Mike Wickett <mike@sagecomm.com>
2016-03-15 22:55:06 -0400

    Added node-slackbots

---
+++ index.js
@@ -0,0 +1,8 @@
+var SlackBot = require('slackbots');
+
+//create a bot
+// See npmjs.com/package/slackbots
+var bot = new SlackBot({
+  token: 'xoxb-27033721031-qq7eszKVbRemOvVQvRYAJJM0',
+  name: 'Squarestatus'
+});
