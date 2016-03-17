3f9bc885551bf97c9fd9ecce595c3a86a9641ec0
Mike Wickett <mike@sagecomm.com>
2016-03-16 22:00:27 -0400

    It's alive! Managed to get the status description and push it to Slack

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
+const https = require('https');
+
 var SlackBot = require('slackbots');
 var token_data = require('./priv-data');
-var sqsp_url = `https://1jkhm1drpysj.statuspage.io/api/v2/summary.json`
+var sqsp_url = "https://1jkhm1drpysj.statuspage.io/api/v2/summary.json";
+var bad_url = "https://1jkhm1rpysj.statuspage.io/api/v2/summary.json";
 
-create a bot
-See npmjs.com/package/slackbots
+//create a bot
+//See npmjs.com/package/slackbots
 var bot = new SlackBot({
+  // Save token "securely" (at least not push it to GitHub)
   token: token_data[0],
   name: 'Squarestatus'
 });
 
-console.log(token_data[0]);
+//Print out error messages
+function printError(error){
+    console.error(error.message);
+}
 
 
-// Save token "securely" (at least not push it to GitHub)
+// Call status page end point at jkhm1drpysj.statuspage.io/api/v2/summary.json
+function check(url) {
+var request = https.get(url, function(res){
+    var body = '';
 
-// Call status page end point at https://1jkhm1drpysj.statuspage.io/api/v2/summary.json
-// Store response
-// Parse response looking for statuses of V5 and V7
+    res.on('data', function(chunk){
+      body += chunk;
+    });
+  // Store response
+    res.on('end', function(){
+      if(res.statusCode === 200) {
+        try {
+          //parse the data
+          var statusResponse = JSON.parse(body);
+          var status = statusResponse.status.description;
+          var message = "Squarespace says: " + status;
+          console.log(message);
+          bot.postMessageToUser('mike', message);
+        } catch(error) {
+          //parse error
+          printError(error);
+        }
+      } else {
+        //Status code error
+        printError({message: "There was an error getting the status "  + https.STATUS_CODES[res.statusCode] });
+      }
+    });
+  }).on('error', printError);
+}
 
-// Logic to check for status 
+check(sqsp_url);
